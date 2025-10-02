442a1f23309514cb754f905cb2f916111ab94525
vaishali1610 <vaishali.balasubramaniam@psiog.com>
2025-10-02 12:15:28 +0530

    polymorphism done

--- module3.cs
+++ module3.cs
@@ -161,46 +161,122 @@ class Program
     }
     //------------------------abstraction-------------------
     abstract class Shape
-{
-    public string Name { get; set; }
-    abstract public double calculateArea();
-    public void print() => Console.WriteLine("shape");
+    {
+        public string Name { get; set; }
+        abstract public double calculateArea();
+        public void print() => Console.WriteLine("shape");
+    }
+    class Circle : Shape
+    {
+        public int Radius { get; set; }
+        public Circle(int radius)
+        {
+            Radius = radius;
+            Name = "circle";
+        }
+        public override double calculateArea()
+        {
+            Console.WriteLine(Name);
+            return Radius * Radius * 3.14;
+        }
+    }
+    class Rectangle : Shape
+    {
+        public int Length { get; set; }
+        public Rectangle(int length)
+        {
+            Length = length;
+            Name = "Rectangle";
+        }
+        public override double calculateArea()
+        {
+            Console.WriteLine(Name);
+            return Length * 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Console.WriteLine(c.calculateArea());
+            Rectangle r = new Rectangle(5);
+            Console.WriteLine(r.calculateArea());
+        }
+    }
+---------------------interface------------------------------------
+interface Shape
+    {
+        string Name { get; set; }
+        double calculateArea();
+        void print();
+    }
+    class Circle : Shape
+    {
+        public string Name { get; set; }
+        public int Radius { get; set; }
+        public Circle(int radius)
+        {
+            Radius = radius;
+            Name = "circle";
+        }
+        public double calculateArea()
+        {
+            Console.WriteLine(Name);
+            return Radius * Radius * 3.14;
+        }
+        public void print() => Console.Write("circle");
+    }
+    class Rectangle : Shape
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public Rectangle(int length)
+        {
+            Length = length;
+            Name = "Rectangle";
+        }
+        public double calculateArea()
+        {
+            Console.WriteLine(Name);
+            return Length * 2;
+        }
+        public void print() => Console.WriteLine("rectangle");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Console.WriteLine(c.calculateArea());
+            Rectangle r = new Rectangle(5);
+            Console.WriteLine(r.calculateArea());
+        }
+    }
+    //----------------polymorphism 
+    class Overloading
+{ 
+    public virtual void operate(int a,int b)=>Console.WriteLine(a+b);
+    public void method() => Console.WriteLine("no parameters");
+    public void method(int n) => Console.WriteLine(n);
+    public void method(string name)=> Console.WriteLine(name);
+    public void method(string name, int age)=> Console.WriteLine(name+" "+age);
 }
-class Circle : Shape
+class Overrides : Overloading
 {
-    public int Radius { get; set; }
-    public Circle(int radius)
-    {
-     Radius = radius;
-     Name = "circle";
-    }
-    public override double calculateArea()
-    {
-        Console.WriteLine(Name);
-        return Radius * Radius * 3.14;
-    }
-}
-class Rectangle : Shape {
- public int Length {  get; set; }
-    public Rectangle(int length)
-    {
-        Length = length;
-        Name = "Rectangle";
-    }
-    public override double calculateArea()
-    {
-        Console.WriteLine(Name);
-        return Length * 2;
-    }
+    public override void operate(int a,int b)=>Console.Write(a*b);
 }
 class Program
 {
     static void Main(string[] args)
     {
-        Circle c = new Circle(5);
-        Console.WriteLine(c.calculateArea());
-        Rectangle r = new Rectangle(5);
-        Console.WriteLine(r.calculateArea());
+        Overloading o = new Overloading();
+        Overrides ov = new Overrides();
+        o.operate(1, 2);
+        ov.operate(1, 2);
+        o.method();
+        o.method("jaisree");
+        o.method("subha", 22);
     }
 }
 
