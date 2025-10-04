8568560180de56f8a94279290ef9e5ac71c707c2
vaishali1610 <vaishali.balasubramaniam@psiog.com>
2025-10-04 22:42:44 +0530

    thread safe singleton

--- singleton.cs
+++ singleton.cs
@@ -1,36 +1,37 @@
 using System;
-
-//class Singleton
-//{
-//    private static Singleton instance;
-//    private Singleton()
-//    {
-//        Console.WriteLine("Singleton instance created.");
-//    }
-//    public static Singleton GetInstance()
-//    {
-//        if (instance == null)
-//        {
-//            instance = new Singleton(); 
-//        }
-//        return instance;
-//    }
-//    public void ShowMessage()
-//    {
-//        Console.WriteLine("Hello from Singleton!");
-//    }
-//}
-//class Program
-//{
-//    static void Main()
-//    {
-//        Singleton s1 = Singleton.GetInstance();
-//        Singleton s2 = Singleton.GetInstance();
-//        s1.ShowMessage();
-//        s2.ShowMessage();
-//        Console.WriteLine(object.ReferenceEquals(s1, s2)); // True, both are same instance
-//    }
-//}
+using System.Threading;
+using System.Threading.Tasks;
+class Singleton
+{
+    private static Singleton instance;
+    private Singleton()
+    {
+        Console.WriteLine("Singleton instance created.");
+    }
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    public void ShowMessage()
+    {
+        Console.WriteLine("Hello from Singleton!");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+        s1.ShowMessage();
+        s2.ShowMessage();
+        Console.WriteLine(object.ReferenceEquals(s1, s2)); // True, both are same instance
+    }
+}
 
 
 class Logger
@@ -70,5 +71,96 @@ class Program
         logger1.Log("First message");
         logger2.Log("Second message");
         Console.WriteLine(object.ReferenceEquals(logger1, logger2)); // True
+    }
+}
+
+class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObj = new object();
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created.");
+    }
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                    instance = new Logger();
+            }
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] {message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Thread[] threads = new Thread[5]; //creating an array of threads
+        for (int i = 0; i < 5; i++)
+        {
+            int threadNum = i + 1;
+            threads[i] = new Thread(() =>
+            {
+                Logger logger = Logger.GetInstance();
+                logger.Log($"Message from thread {threadNum}");
+            });
+            threads[i].Start(); //thread starts running
+        }
+        // Wait for all threads to finish
+        foreach (var t in threads)
+            t.Join();
+        Console.WriteLine(object.ReferenceEquals(threads[1], threads[2]));
+    }
+}
+class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObj = new object();
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created.");
+    }
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                    instance = new Logger();
+            }
+        }
+        return instance;
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine($"[{Task.CurrentId}] {message}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Task[] tasks = new Task[5];
+        for (int i = 0; i < 5; i++)
+        {
+            int taskNum = i + 1;
+            tasks[i] = Task.Run(() =>
+            {
+                Logger logger = Logger.GetInstance();
+                logger.Log($"Message from task {taskNum}");
+            });
+        }
+        Task.WaitAll(tasks);
     }
 }
